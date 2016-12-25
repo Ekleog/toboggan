11044d5bffe5eb05ead4d13d8a5f78c8dbcc6220
Léo Gaspard <leo@gaspard.io>
2016-12-25 22:44:48 +0100

    Give five args to each prctl call

--- src/seccomp.rs
+++ src/seccomp.rs
@@ -10,13 +10,13 @@ const SECCOMP_MODE_FILTER: c_int = 2;
 
 pub fn has_seccomp() -> bool {
     unsafe {
-        prctl(PR_GET_SECCOMP) == 0
+        prctl(PR_GET_SECCOMP, 0, 0, 0, 0) == 0
     }
 }
 
 pub fn has_seccomp_filter() -> bool {
     unsafe {
-        let res = prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, 0) < 0
+        let res = prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, 0, 0, 0) < 0
             && *__errno_location() == EFAULT;
         *__errno_location() = 0;
         res
@@ -40,10 +40,10 @@ struct sock_fprog {
 pub fn install_filter(filter: bpf::Filter) -> Result<(), &'static str> {
     let mut filter = vec![sock_filter { code: 0x06, jt: 0, jf: 0, k: 0 }];
     unsafe {
-        let prog = sock_fprog { len: 1, filter: filter[..].as_mut_ptr() };
-        if prctl(PR_SET_NO_NEW_PRIVS, 1) != 0 {
+        let prog = sock_fprog { len: filter.len() as c_ushort, filter: filter[..].as_mut_ptr() };
+        if prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) != 0 {
             Err("prctl(PR_SET_NO_NEW_PRIVS)")
-        } else if prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog) != 0 {
+        } else if prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog, 0, 0) != 0 {
             Err("prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, [filter])")
         } else {
             Ok(())
