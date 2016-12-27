f56c2b9b25d42edc51e3d190c43ae2d44da5a7ce
Léo Gaspard <leo@gaspard.io>
2016-12-27 02:02:34 +0100

    Try using ptrace to log syscalls

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod syscalls;
 
 use syscalls::Syscall;
 
+// TODO: check things still work (or not) after switch to kernel 4.8 (cf. man 2 ptrace)
+
 fn spawn_child(sigset: libc::sigset_t) {
     posix::ptraceme();
 
@@ -15,13 +17,33 @@ fn spawn_child(sigset: libc::sigset_t) {
         panic!("unable to install seccomp filter: {}", e);
     }
 
-    posix::exec("/run/current-system/sw/bin/ls");
+    println!("about to spawn ls!");
+
+    posix::exec("ls", &["ls"]);
 
     panic!("Unable to spawn ls: {}", unsafe { *libc::__errno_location() });
 }
 
 fn ptrace_child(pid: libc::pid_t) {
     posix::ptracehim(pid);
+
+    println!("Entering infinite loop");
+    loop {
+        // TODO: manage multiprocess
+        let status = posix::waitit();
+        if posix::is_seccomp(status) {
+            let syscall = posix::syscall_number(pid);
+            println!("Syscall with code {}", syscall);
+        } else if posix::is_exit(status) {
+            println!("Exit!");
+            break
+        } else if posix::is_exec(status) {
+            println!("Exec!");
+        } else {
+            println!("Out of waitit with unknown status 0x{:08x}", status);
+        }
+        posix::continueit(pid);
+    }
 }
 
 fn main() {
--- src/posix.rs
+++ src/posix.rs
@@ -1,12 +1,22 @@
 use std::{ffi, mem};
 use libc::*;
 
-pub fn exec(prog: &str) {
+const PTRACE_EVENT_EXEC: c_int = 4;
+const PTRACE_EVENT_SECCOMP: c_int = 7;
+const ORIG_RAX: c_int = 120;
+
+pub fn exec(prog: &str, argv: &[&str]) {
     let prog = ffi::CString::new(prog).unwrap();
-    let argv = [0 as *const c_char];
-    let envp = [0 as *const c_char];
+    let mut args: Vec<*const c_char> = Vec::new();
+    for arg in argv {
+        println!("pushing '{}'", arg);
+        args.push(ffi::CString::new(arg.clone()).unwrap().as_ptr());
+    }
+    args.push(0 as *const c_char);
+    println!("args: {:?}", args);
+    // TODO: allow to block environment passing?
     unsafe {
-        execve(prog.as_ptr(), argv.as_ptr(), envp.as_ptr());
+        execvp(prog.as_ptr(), args.as_ptr());
     }
 }
 
@@ -54,13 +64,58 @@ fn sendcont(pid: pid_t) {
 }
 
 pub fn ptraceme() {
-    unsafe {
-        ptrace(PTRACE_TRACEME, 0, 0, 0);
-    }
     waitforcont();
 }
 
+pub fn waitit() -> c_int {
+    unsafe {
+        let mut status: c_int = mem::uninitialized();
+        wait(&mut status);
+        status
+    }
+}
+
+pub fn continueit(pid: pid_t) {
+    unsafe {
+        ptrace(PTRACE_CONT, pid, 0, 0);
+    }
+}
+
 pub fn ptracehim(pid: pid_t) {
-    // TODO: actually ptrace him
+    if unsafe { ptrace(PTRACE_ATTACH, pid, 0, 0) } != 0 {
+        panic!("unable to ptrace child!");
+    }
+    waitit(); // Wait for the process to receive the SIGSTOP
+    // TODO: Make sure forks are ptraced
+    let options = PTRACE_O_TRACESECCOMP | PTRACE_O_TRACEEXEC;
+    if unsafe { ptrace(PTRACE_SETOPTIONS, pid, 0, options) } != 0 {
+        panic!("unable to trace seccomp on child: {}", unsafe { *__errno_location() });
+    }
+    continueit(pid);
     sendcont(pid);
+}
+
+pub fn syscall_number(pid: pid_t) -> i64 {
+    unsafe {
+        let old_errno = *__errno_location();
+        *__errno_location() = 0;
+        let res = ptrace(PTRACE_PEEKUSER, pid, ORIG_RAX, 0);
+        if *__errno_location() != 0 {
+            panic!("Unable to peekuser: {}", *__errno_location()); // TODO: Remove this and cleanly handle error
+        }
+        *__errno_location() = old_errno;
+        res
+    }
+}
+
+pub fn is_seccomp(status: c_int) -> bool {
+    status >> 8 == (SIGTRAP | (PTRACE_EVENT_SECCOMP << 8))
+}
+
+pub fn is_exit(status: c_int) -> bool {
+    status & 0x7f == 0
+}
+
+pub fn is_exec(status: c_int) -> bool {
+    status >> 8 == (SIGTRAP | (PTRACE_EVENT_EXEC << 8))
 }
